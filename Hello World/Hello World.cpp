67d859345397ba3c2d71f04138ad12b6828b0cca
Sean <shonwr@gmail.com>
2019-10-05 20:30:48 -0700

    Add project files.

---
+++ Hello World/Hello World.cpp
@@ -0,0 +1,15 @@
+// Hello World.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World";
+
+	getchar();
+    return 0;
+}
+
